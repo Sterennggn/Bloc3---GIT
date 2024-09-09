0d10fb4659405b627d6fb2060013abc276518877
SGOUGEON2 <sterenn.gougeon@gmail.com>
2024-09-09 11:07:14 +0200

    l

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const hostname = '127.0.0.1'
+const port = 8000
+
+const server =
+    http.createServer( (requete, response) => {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/plain')
+        response.end('Coucou les champions SLAM2')
+    })
+
+server.listen(port, hostname, () => {
+    console.log(`Le serveur tourne sur mon poste : http://${hostname} sur le port ${port}/`)
+})
